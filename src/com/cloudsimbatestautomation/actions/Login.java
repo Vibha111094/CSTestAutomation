973f2dd7cb8040eaedcf4a7232e9e80ca13a9a00
Vibha V Rao <Vibha.vr@betsol.com>
2016-01-13 18:51:45 +0530

    Initial commit of Eclipse project with helloworld template

---
+++ src/com/cloudsimbatestautomation/actions/Login.java
@@ -0,0 +1,11 @@
+package com.cloudsimbatestautomation.actions;
+
+public class Login {
+	//Take login creds from a props file
+	
+	public boolean tryLogin(){
+		//Try logging in using selenium
+		//Return true for successfull login
+		return true;
+	}
+}
